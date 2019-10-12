69f8aa5002067f5f565c38ffd5b1abea1e5fe083
AsociTon <asociton@outlook.com>
2019-10-13 00:29:28 +0530

    Add morgan for generating logs by default.
    Adding morgan to app.js and adding the required
    dependency to the readme.md .

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RESTful-API
 Creating a REpresentational State Transfer API using node.js
+
+## Installing the dependencies
+Use npm install on your machine to install the following dependencies
+* express
+* nodemon ( dev dependency ) > npm install --save--dev nodemon
+* morgan ( for listing the logs by default of the restfulapi )
--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express"); // Express Application e.g. Express makes handling requests a bit easier
 
 const app = express(); // Spins up express as a function which makes all the utilities of an express app available to us
-
-//use sets up a middleware
+const morgan = require("morgan"); // By default shows the logs for the app
+// Use sets up a middleware
 
 /*
 app.use((req, res, next) => {
@@ -17,6 +17,8 @@ app.use((req, res, next) => {
 const productRoute = require('./api/products'); // Import the products route
 const orderRoute = require('./api/order'); //Import the order route
 
-app.use('/products',productRoute); // every request to /products is handled by productRoute (api/products.js file)
+app.use(morgan('dev')); // Middleware for showing the logs by default using the morgan dependecy 
+
+app.use('/products',productRoute); // Every request to /products is handled by productRoute (api/products.js file)
 app.use('/order', orderRoute);
 module.exports = app;
