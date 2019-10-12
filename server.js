9830d1efc5c637e9ca3d788ce9dd22c11e345248
AsociTon <asociton@outlook.com>
2019-10-12 15:48:42 +0530

    Add basic node.js server

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require("http"); // require is the node way to import
+
+const port = process.env.PORT || 7000; // to run on the default provided port or else on port 7000
+
+const server = http.createServer(); // to create the server
+
+server.listen(port); // to make the server start listening to the requests on the assigned port
